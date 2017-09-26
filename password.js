ad8649a72739ee23c3e735c94272756754db1536
github username <github email address>
2017-09-26 18:24:56 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,20 @@
 // code solution here
+process.argv.shift()
+process.argv.shift()
+let length = parseInt(process.argv[0])
+let num = parseInt(process.argv[1])
+let chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+let passwords = []
+
+for (i = 0; i < num; i++) {
+  let randomstring = '';
+  for (j = 0; j < length; j++) {
+    let rnum = Math.floor(Math.random() * chars.length)
+    randomstring += chars.substring(rnum, rnum+1);
+    debugger
+  }
+  passwords.push(randomstring)
+}
+console.log(passwords[0])
+// console.log(passwords[0])
+process.exit(1)
