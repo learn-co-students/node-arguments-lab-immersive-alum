aeb2b97a68f110df8f7daa933ba53eff42f2f14b
yfern328 <yfernan2@u.rochester.edu>
2017-11-21 17:16:05 -0500

    completed lab. created password generator and process.argv arguments

--- password.js
+++ password.js
@@ -1,1 +1,20 @@
-// code solution here
+var length = process.argv[2]
+var times = process.argv[3] || 1
+
+const generatePassword = (length=10) => {
+
+  let text = "";
+  let key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++) {
+    text += key[(Math.floor(Math.random() * key.length))]
+  }
+
+  return text;
+}
+
+for (var i = 0; i < times; i++){
+    console.log(generatePassword(length))
+}
+
+process.exit(0)
