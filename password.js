b672cd0897e635a7cd1c4f493eed22c546aa5b05
Mike <zluo@alumni.berklee.edu>
2017-11-28 17:15:25 -0500

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,11 @@
-// code solution here
+const randomstring = require('randomstring')
+
+process.argv.shift()
+process.argv.shift()
+
+let n = 0
+while (n < parseInt(process.argv[1])) {
+  console.log(randomstring.generate(parseInt(process.argv[0])))
+}
+
+process.exit(0)
