63000c12fdd9814a5fb78396ad538fa7ed345a3d
Jon <jbvaldesuso@gmail.com>
2018-07-23 15:57:46 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,12 @@
 // code solution here
+const x = process.argv[2]
+const y = process.argv[3]
+const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz"
+
+for(let i = 0; i < y; i++) {
+  let password = ''
+  for(let j = 0; j < x; j++) {
+    password += chars[Math.floor(Math.random() * chars.length)]
+  }
+  console.log(password)
+}
