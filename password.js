451299e26e6647f3e5b9eac1a31675414bec3c8d
alin684 <al684@cornell.edu>
2018-08-13 23:58:32 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,9 @@
-// code solution here
+var passLength = process.argv[2]
+var passNum = process.argv[3]
+
+for (var i = 0; i <= passNum; i++) {
+  console.log(Array(Number(passLength)+1).join(i))
+  done()
+}
+
+process.exit(0)
