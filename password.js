3c6d889096a4340939ec73c1a00fef0e0658e638
github username <github email address>
2018-09-06 23:15:49 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,20 @@
 // code solution here
+let leng = process.argv[2]
+let numOfPasswords = process.argv[3]
+let i = 0
+let passwords = []
+ const makePassword = (leng) => {
+  let password = ''
+  let j = 0
+  while(j < leng){
+    password += 'a'
+    j++
+  }
+  return password
+}
+ while(i < numOfPasswords){
+  passwords.push(makePassword(leng))
+  i++
+}
+ console.log(passwords[0])
+process.exit(1)
