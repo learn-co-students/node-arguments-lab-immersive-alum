b3ecb677f1c42363dce2f655d5f6ac3c5d273187
geewey <geewey@gmail.com>
2020-07-21 16:11:44 +0800

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,27 @@
-// code solution here
+process.argv.shift();
+process.argv.shift();
+
+let randomPasswords = [];
+let validCharacters = "abcdefghijklmnopqrstuvwxyz1234567890";
+if (!process.argv[1]) {
+  console.log("Error!");
+  return "Error!";
+}
+
+let passwordLength = process.argv[0];
+let passwordNumberToCreate = process.argv[1];
+
+const generatePassword = (passwordLength) => {
+  let password = [];
+  let chars = validCharacters.split("");
+  while (password.length < passwordLength) {
+    password.push(chars[2]);
+  }
+  randomPasswords.push(password.join(""));
+};
+
+for (let i = 1; i <= passwordNumberToCreate; i++) {
+  generatePassword(passwordLength);
+}
+
+randomPasswords.forEach((x) => console.log(x));
